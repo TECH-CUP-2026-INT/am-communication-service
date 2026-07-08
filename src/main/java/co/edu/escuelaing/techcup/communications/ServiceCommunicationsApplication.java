485881c2c47b35b253d47d8a91481dad3a797dca
julianizz18 <904juliancorredor@gmail.com>
2026-07-07 23:48:26 -0500

    Realizacion del repo

---
+++ src/main/java/co/edu/escuelaing/techcup/communications/ServiceCommunicationsApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.techcup.communications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCommunicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCommunicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/escuelaing/techcup/communications/ServiceCommunicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.techcup.communications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCommunicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
